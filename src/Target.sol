b47842f890faec91a6c9ff1307079ffc53f2920b
Zaryab <zaryabafser2000@gmail.com>
2025-04-28 19:12:31 +0400

    target+storageProof

---
+++ src/Target.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Target {
+    uint256 public value;
+    mapping (address => uint) public valueLockedNative;
+    mapping (address => uint) public valueLockedToken;
+
+    // events
+    event ValueUpdate(uint256 oldValue, uint256 newValue);
+    event ValueLockedNative(address indexed user, uint256 amount);
+    event ValueLockedToken(address indexed user, uint256 amount);
+
+    function updateValue(uint256 _val) external {
+        uint256 _oldValue = value;
+        value = _val;
+
+        emit ValueUpdate(_oldValue, value);   
+    }
+
+    function lockNativeToken(address _user) payable external {
+        require(_user != address(0), "User address cannot be zero");
+        require(msg.value > 0, "Value must be greater than 0");
+        valueLockedNative[_user] += msg.value;
+
+        emit ValueLockedNative(_user, msg.value); 
+    }
+
+    function lockToken(address _user, uint256 _amount, address _token) external {
+        require(_user != address(0), "User address cannot be zero");
+        require(_amount > 0, "Value must be greater than 0");
+        require(_token != address(0), "Token address cannot be zero");
+
+        IERC20 token = IERC20(_token);
+        token.transferFrom(msg.sender, address(this), _amount);
+        valueLockedNative[_user] += _amount; 
+        
+        emit ValueLockedToken(_user, _amount);
+    }
+}
